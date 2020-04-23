c1c64a3ddc6ead055732cb14436578594cbf62dd
Mikolaj <mikolaj.mlynarczyk@icloud.com>
2020-04-23 15:35:56 +0200

    Dodano projekt (czujnik temp. + LCD)
    Projekt wyświetlający temperaturę pobraną z czujnika DS18B20 na wyświetlaczu LCD 16x2)

---
+++ temp2/Core/Src/STM_MY_LCD16X2.c
@@ -0,0 +1,336 @@
+#include "STM_MY_LCD16X2.h"
+
+static const uint32_t writeTimeConstant = 10;
+static uint8_t mode_8_4_I2C = 1;
+static GPIO_TypeDef* PORT_RS_and_E;							  // RS and E PORT
+static uint16_t PIN_RS, PIN_E;									  // RS and E pins
+static GPIO_TypeDef* PORT_LSB;										// LSBs D0, D1, D2 and D3 PORT
+static uint16_t D0_PIN, D1_PIN, D2_PIN, D3_PIN;	// LSBs D0, D1, D2 and D3 pins
+static GPIO_TypeDef* PORT_MSB;										// MSBs D5, D6, D7 and D8 PORT
+static uint16_t D4_PIN, D5_PIN, D6_PIN, D7_PIN;	// MSBs D5, D6, D7 and D8 pins
+
+static uint8_t DisplayControl = 0x0F;
+static uint8_t FunctionSet = 0x38;
+
+
+static void LCD1602_EnablePulse(void)
+{
+	HAL_GPIO_WritePin(PORT_RS_and_E, PIN_E, GPIO_PIN_SET);
+	delay(writeTimeConstant);
+	HAL_GPIO_WritePin(PORT_RS_and_E, PIN_E, GPIO_PIN_RESET);
+	delay(60);
+}
+
+
+static void LCD1602_RS(bool state)
+{
+	if(state) HAL_GPIO_WritePin(PORT_RS_and_E, PIN_RS, GPIO_PIN_SET);
+	else HAL_GPIO_WritePin(PORT_RS_and_E, PIN_RS, GPIO_PIN_RESET);
+}
+
+
+
+static void LCD1602_write(uint8_t byte)
+{
+	uint8_t LSB_nibble = byte&0xF, MSB_nibble = (byte>>4)&0xF;
+	 
+	if(mode_8_4_I2C == 1)		//8bits mode
+	{
+		//write data to output pins
+		//LSB data
+		HAL_GPIO_WritePin(PORT_LSB, D0_PIN, (GPIO_PinState)(LSB_nibble&0x1));
+		HAL_GPIO_WritePin(PORT_LSB, D1_PIN, (GPIO_PinState)(LSB_nibble&0x2));
+		HAL_GPIO_WritePin(PORT_LSB, D2_PIN, (GPIO_PinState)(LSB_nibble&0x4));
+		HAL_GPIO_WritePin(PORT_LSB, D3_PIN, (GPIO_PinState)(LSB_nibble&0x8));
+		//MSB data
+		HAL_GPIO_WritePin(PORT_MSB, D4_PIN, (GPIO_PinState)(MSB_nibble&0x1));
+		HAL_GPIO_WritePin(PORT_MSB, D5_PIN, (GPIO_PinState)(MSB_nibble&0x2));
+		HAL_GPIO_WritePin(PORT_MSB, D6_PIN, (GPIO_PinState)(MSB_nibble&0x4));
+		HAL_GPIO_WritePin(PORT_MSB, D7_PIN, (GPIO_PinState)(MSB_nibble&0x8));
+		//Write the Enable pulse
+		LCD1602_EnablePulse();
+	}
+	else if(mode_8_4_I2C == 2)	//4 bits mode
+	{
+		//write data to output pins
+		//MSB data
+		HAL_GPIO_WritePin(PORT_MSB, D4_PIN, (GPIO_PinState)(MSB_nibble&0x1));
+		HAL_GPIO_WritePin(PORT_MSB, D5_PIN, (GPIO_PinState)(MSB_nibble&0x2));
+		HAL_GPIO_WritePin(PORT_MSB, D6_PIN, (GPIO_PinState)(MSB_nibble&0x4));
+		HAL_GPIO_WritePin(PORT_MSB, D7_PIN, (GPIO_PinState)(MSB_nibble&0x8));
+		//Write the Enable pulse
+		LCD1602_EnablePulse();
+		
+		//LSB data
+		HAL_GPIO_WritePin(PORT_MSB, D4_PIN, (GPIO_PinState)(LSB_nibble&0x1));
+		HAL_GPIO_WritePin(PORT_MSB, D5_PIN, (GPIO_PinState)(LSB_nibble&0x2));
+		HAL_GPIO_WritePin(PORT_MSB, D6_PIN, (GPIO_PinState)(LSB_nibble&0x4));
+		HAL_GPIO_WritePin(PORT_MSB, D7_PIN, (GPIO_PinState)(LSB_nibble&0x8));
+		//Write the Enable pulse
+		LCD1602_EnablePulse();
+	}
+}
+
+
+static void LCD1602_writeCommand(uint8_t command)
+{
+	LCD1602_RS(false);
+	LCD1602_write(command);
+}
+
+
+static void LCD1602_writeData(uint8_t data)
+{
+	LCD1602_RS(true);
+	LCD1602_write(data);
+}
+
+
+static void LCD1602_write4bitCommand(uint8_t nibble)
+{
+	uint8_t LSB_nibble = nibble&0xF;
+	//Set RS to 0
+	LCD1602_RS(false);
+	//LSB data
+	HAL_GPIO_WritePin(PORT_MSB, D4_PIN, (GPIO_PinState)(LSB_nibble&0x1));
+	HAL_GPIO_WritePin(PORT_MSB, D5_PIN, (GPIO_PinState)(LSB_nibble&0x2));
+	HAL_GPIO_WritePin(PORT_MSB, D6_PIN, (GPIO_PinState)(LSB_nibble&0x4));
+	HAL_GPIO_WritePin(PORT_MSB, D7_PIN, (GPIO_PinState)(LSB_nibble&0x8));
+	//Write the Enable pulse
+	LCD1602_EnablePulse();
+}
+
+
+void LCD1602_Begin8BIT(GPIO_TypeDef* PORT_RS_E, uint16_t RS, uint16_t E, GPIO_TypeDef* PORT_LSBs0to3, uint16_t D0, uint16_t D1, uint16_t D2, uint16_t D3, GPIO_TypeDef* PORT_MSBs4to7, uint16_t D4, uint16_t D5, uint16_t D6, uint16_t D7)
+{
+	//Set GPIO Ports and Pins data
+	PORT_RS_and_E = PORT_RS_E;
+	PIN_RS = RS;
+	PIN_E = E;
+	PORT_LSB = PORT_LSBs0to3;
+	D0_PIN = D0;
+	D1_PIN = D1;
+	D2_PIN = D2;
+	D3_PIN = D3;
+	PORT_MSB = PORT_MSBs4to7;
+	D4_PIN = D4;
+	D5_PIN = D5;
+	D6_PIN = D6;
+	D7_PIN = D7;
+	//Set the mode to 8 bits
+	mode_8_4_I2C = 1;
+	//Function set variable to 8 bits mode
+	FunctionSet = 0x38;
+	
+	//Initialise LCD
+	//1. Wait at least 15ms
+	HAL_Delay(20);
+	//2. Attentions sequence
+	LCD1602_writeCommand(0x30);
+	HAL_Delay(5);
+	LCD1602_writeCommand(0x30);
+	HAL_Delay(1);
+	LCD1602_writeCommand(0x30);
+	HAL_Delay(1);
+	//3. Function set; Enable 2 lines, Data length to 8 bits
+	LCD1602_writeCommand(LCD_FUNCTIONSET | LCD_FUNCTION_N | LCD_FUNCTION_DL);
+	//4. Display control (Display ON, Cursor ON, blink cursor)
+	LCD1602_writeCommand(LCD_DISPLAYCONTROL | LCD_DISPLAY_B | LCD_DISPLAY_C | LCD_DISPLAY_D);
+	//5. Clear LCD and return home
+	LCD1602_writeCommand(LCD_CLEARDISPLAY);
+	HAL_Delay(2);
+}
+
+
+void LCD1602_Begin4BIT(GPIO_TypeDef* PORT_RS_E, uint16_t RS, uint16_t E, GPIO_TypeDef* PORT_MSBs4to7, uint16_t D4, uint16_t D5, uint16_t D6, uint16_t D7)
+{
+	//Set GPIO Ports and Pins data
+	PORT_RS_and_E = PORT_RS_E;
+	PIN_RS = RS;
+	PIN_E = E;
+	PORT_MSB = PORT_MSBs4to7;
+	D4_PIN = D4;
+	D5_PIN = D5;
+	D6_PIN = D6;
+	D7_PIN = D7;
+	//Set the mode to 4 bits
+	mode_8_4_I2C = 2;
+	//Function set variable to 4 bits mode
+	FunctionSet = 0x28;
+	
+	//Initialise LCD
+	//1. Wait at least 15ms
+	HAL_Delay(20);
+	//2. Attentions sequence
+	LCD1602_write4bitCommand(0x3);
+	HAL_Delay(5);
+	LCD1602_write4bitCommand(0x3);
+	HAL_Delay(1);
+	LCD1602_write4bitCommand(0x3);
+	HAL_Delay(1);
+	LCD1602_write4bitCommand(0x2);  //4 bit mode
+	HAL_Delay(1);
+	//3. Display control (Display ON, Cursor ON, blink cursor)
+	LCD1602_writeCommand(LCD_DISPLAYCONTROL | LCD_DISPLAY_B | LCD_DISPLAY_C | LCD_DISPLAY_D);
+	//4. Clear LCD and return home
+	LCD1602_writeCommand(LCD_CLEARDISPLAY);
+	HAL_Delay(3);
+	//4. Function set; Enable 2 lines, Data length to 8 bits
+	LCD1602_writeCommand(LCD_FUNCTIONSET | LCD_FUNCTION_N);
+	HAL_Delay(3);
+}
+
+
+void LCD1602_print(char string[])
+{
+	for(uint8_t i=0;  i< 16 && string[i]!=NULL; i++)
+	{
+		LCD1602_writeData((uint8_t)string[i]);
+	}
+}
+
+
+void LCD1602_setCursor(uint8_t row, uint8_t col)
+{
+	uint8_t maskData;
+	maskData = (col-1)&0x0F;
+	if(row==1)
+	{
+		maskData |= (0x80);
+		LCD1602_writeCommand(maskData);
+	}
+	else
+	{
+		maskData |= (0xc0);
+		LCD1602_writeCommand(maskData);
+	}
+}
+
+
+void LCD1602_1stLine(void)
+{
+	LCD1602_setCursor(1,1);
+}
+
+
+void LCD1602_2ndLine(void)
+{
+	LCD1602_setCursor(2,1);
+}
+
+
+void LCD1602_TwoLines(void)
+{
+	FunctionSet |= (0x08);
+	LCD1602_writeCommand(FunctionSet);
+}
+
+
+void LCD1602_OneLine(void)
+{
+	FunctionSet &= ~(0x08);
+	LCD1602_writeCommand(FunctionSet);
+}
+
+
+void LCD1602_noCursor(void)
+{
+	DisplayControl &= ~(0x02);
+	LCD1602_writeCommand(DisplayControl);
+}
+
+
+void LCD1602_cursor(void)
+{
+	DisplayControl |= (0x02);
+	LCD1602_writeCommand(DisplayControl);
+}
+
+
+void LCD1602_clear(void)
+{
+	LCD1602_writeCommand(LCD_CLEARDISPLAY);
+	HAL_Delay(3);
+}
+
+
+void LCD1602_noBlink(void)
+{
+	DisplayControl &= ~(0x01);
+	LCD1602_writeCommand(DisplayControl);
+}
+void LCD1602_blink(void)
+{
+	DisplayControl |= 0x01;
+	LCD1602_writeCommand(DisplayControl);
+}
+
+
+void LCD1602_noDisplay(void)
+{
+	DisplayControl &= ~(0x04);
+	LCD1602_writeCommand(DisplayControl);
+}
+void LCD1602_display(void)
+{
+	DisplayControl |= (0x04);
+	LCD1602_writeCommand(DisplayControl);
+}
+
+
+void LCD1602_shiftToRight(uint8_t num)
+{
+	for(uint8_t i=0; i<num;i++)
+	{
+		LCD1602_writeCommand(0x1c);
+	}
+}
+
+
+void LCD1602_shiftToLeft(uint8_t num)
+{
+	for(uint8_t i=0; i<num;i++)
+	{
+		LCD1602_writeCommand(0x18);
+	}
+}
+
+
+void LCD1602_PrintInt(int number)
+{
+	char numStr[16];
+	sprintf(numStr,"%d", number);
+	LCD1602_print(numStr);
+}
+
+
+void reverse(char* str, int len)
+{
+    int i = 0, j = len - 1, temp;
+    while (i < j) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
+}
+
+
+int intToStr(int x, char str[], int d)
+{
+    int i = 0;
+    while (x) {
+        str[i++] = (x % 10) + '0';
+        x = x / 10;
+    }
+
+    // If number of digits required is more, then
+    // add 0s at the beginning
+    while (i < d)
+        str[i++] = '0';
+
+    reverse(str, i);
+    str[i] = '\0';
+    return i;
+}
---
+++ temp2/Core/Src/main.c
@@ -0,0 +1,399 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdio.h"
+#include "STM_MY_LCD16X2.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim3;
+
+/* USER CODE BEGIN PV */
+uint8_t Presence;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM3_Init(void);
+/* USER CODE BEGIN PFP */
+void delay(uint16_t time){
+	__HAL_TIM_SET_COUNTER(&htim3, 0);
+	while((__HAL_TIM_GET_COUNTER(&htim3))<time);
+}
+
+uint8_t Rh_byte1, Rh_byte2, Temp_byte1, Temp_byte2;
+uint16_t SUM, RH, TEMP;
+
+float Temperature = 0;
+
+void Set_Pin_Output(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin){
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	GPIO_InitStruct.Pin = GPIO_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
+}
+void Set_Pin_Input(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin){
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	GPIO_InitStruct.Pin = GPIO_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
+}
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+#define DS18B20_PORT GPIOA
+#define DS18B20_PIN GPIO_PIN_1
+
+uint8_t DS18B20_Start(void){
+	uint8_t Response = 0;
+	Set_Pin_Output(DS18B20_PORT, DS18B20_PIN);
+	HAL_GPIO_WritePin(DS18B20_PORT, DS18B20_PIN, 0);
+	delay(480);
+
+	Set_Pin_Input(DS18B20_PORT, DS18B20_PIN);
+	delay(80);
+
+	if(!(HAL_GPIO_ReadPin(DS18B20_PORT, DS18B20_PIN))) Response = 1;
+	else Response = -1;
+
+	delay(400);
+
+	return Response;
+}
+
+void DS18B20_Write(uint8_t data){
+	Set_Pin_Output(DS18B20_PORT, DS18B20_PIN);
+
+	for(int i=0; i<8; i++){
+		if((data & (1<<i))!=0){
+			Set_Pin_Output(DS18B20_PORT, DS18B20_PIN);
+			HAL_GPIO_WritePin(DS18B20_PORT, DS18B20_PIN, 0);
+			delay(1);
+
+			Set_Pin_Input(DS18B20_PORT, DS18B20_PIN);
+			delay(60);
+		}
+		else{
+			Set_Pin_Output(DS18B20_PORT, DS18B20_PIN);
+			HAL_GPIO_WritePin(DS18B20_PORT, DS18B20_PIN, 0);
+			delay(60);
+
+			Set_Pin_Input(DS18B20_PORT, DS18B20_PIN);
+		}
+	}
+}
+
+uint8_t DS18B20_Read(void){
+	uint8_t value = 0;
+	Set_Pin_Input(DS18B20_PORT, DS18B20_PIN);
+
+	for(int i=0; i<8; i++){
+		Set_Pin_Output(DS18B20_PORT, DS18B20_PIN);
+
+		HAL_GPIO_WritePin(DS18B20_PORT, DS18B20_PIN, 0);
+		delay(2);
+
+		Set_Pin_Input(DS18B20_PORT, DS18B20_PIN);
+		if(HAL_GPIO_ReadPin(DS18B20_PORT, DS18B20_PIN)){
+			value |= 1 << i;
+		}
+		delay(60);
+	}
+	return value;
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+	  int tmp, tmp2;
+	  float tmp3;
+  /* USER CODE END 1 */
+  
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM3_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_TIM_Base_Start(&htim3);
+
+  LCD1602_Begin4BIT(RS_GPIO_Port, RS_Pin, E_Pin, D4_GPIO_Port, D4_Pin, D5_Pin, D6_Pin, D7_Pin);
+
+  char linia[16];
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  Presence = DS18B20_Start();
+	  HAL_Delay(1);
+	  DS18B20_Write(0xCC);
+	  DS18B20_Write(0x44);
+	  HAL_Delay(800);
+
+	  Presence = DS18B20_Start();
+	  HAL_Delay(1);
+	  DS18B20_Write(0xCC);
+	  DS18B20_Write(0xBE);
+
+	  Temp_byte1 = DS18B20_Read();
+	  Temp_byte2 = DS18B20_Read();
+	  TEMP = (Temp_byte2<<8)|Temp_byte1;
+	  Temperature = (float)TEMP/16;
+
+
+	  tmp = (int)Temperature;
+	  tmp3 = Temperature -tmp;
+	  tmp2 = trunc(tmp3*10);
+	  sprintf(linia, "%d.%d",tmp,tmp2);
+
+	  LCD1602_clear();
+	  LCD1602_setCursor(1,1);
+	  LCD1602_print(linia);
+
+	  if(Temperature > 20 && Temperature < 30) HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, 1);
+	  //HAL_Delay(3000);
+
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage 
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 84-1;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 0xffff-1;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, D4_Pin|D5_Pin|D6_Pin|D7_Pin 
+                          |GPIO_PIN_12, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, RS_Pin|E_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PA1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : D4_Pin D5_Pin D6_Pin D7_Pin 
+                           PD12 */
+  GPIO_InitStruct.Pin = D4_Pin|D5_Pin|D6_Pin|D7_Pin 
+                          |GPIO_PIN_12;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : RS_Pin E_Pin */
+  GPIO_InitStruct.Pin = RS_Pin|E_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
